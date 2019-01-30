86029eb7313a12ce3ca8a3bbeafbcbb4cdad6258
Sonu Giri <sonugiri1043@gmail.com>
2019-01-30 14:25:54 +0530

    Remove dups from linked list

--- 2-Linked-Lists/RemoveDups.cpp
+++ 2-Linked-Lists/RemoveDups.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -14,26 +15,69 @@ private:
   Node *next_;
 public:
   Node( int _data ) : data_( _data ), next_( NULL ) {}
-
   ~Node() {
-    if( next_ != NULL ) {
+    if( next_ != NULL )
       delete next_;
-    }
   }
-
-  Node * next() {
-    return this->next_;
-  }
-
-  int data() {
-    return data_;
-  }
-
-  void nextIs( Node *node ) {
-    this->next_ = node;
-  }
+  Node * next() { return this->next_; }
+  int data() { return data_; }
+  void nextIs( Node *node ) { this->next_ = node; }
 };
 
+void addFront( Node *head, int data ) {
+  Node *newNode = new Node( data );
+  newNode->nextIs( head->next() );
+  head->nextIs( newNode );
+}
+
+void printLL( Node *node ) {
+  while( node != NULL ) {
+    cout<< node->data() << "->";
+    node = node->next();
+  }
+  cout << endl;
+}
+
+void removeDups( Node *node ) {
+  unordered_map< int, bool > keyMap;
+  Node *currNode = node;
+  Node *prev = NULL;
+  while( currNode != NULL ) {
+    if( keyMap.find( currNode->data() ) == keyMap.end() ) {
+      // key not found, insert key in set
+      keyMap.insert( make_pair( currNode->data(), true ) );
+    } else {
+      // key found, remove currNode
+      cout<< "Deleting " << currNode->data() << endl;
+      prev->nextIs( currNode->next() );
+      currNode->nextIs( NULL );
+      delete currNode;
+      currNode = prev;
+    }
+    prev = currNode;
+    currNode = currNode->next();
+  }
+}
+
 int main() {
+  // keep head as a dummy Node
+  Node *head = new Node( -1 );
+
+  // create a LL with some duplicate entries
+  addFront( head, 10 );
+  addFront( head, 12 );
+  addFront( head, 14 );
+  addFront( head, 16 );
+  addFront( head, 10 );
+  addFront( head, 14 );
+  addFront( head, 18 );
+  addFront( head, 10 );
+  
+  // print Linked List
+  printLL( head->next() );
+
+  removeDups( head->next() );
+  
+  printLL( head->next() );
   return 0;
 }
