282c3c7dbe1432bf799dd93a15206ed16cdb9251
Sonu Giri <sonugiri1043@gmail.com>
2019-01-31 20:41:02 +0530

    Three stack with one array

---
+++ 3-Stacks-And-Queues/ThreeStackWithOneArray.cpp
@@ -0,0 +1,22 @@
+/*
+  Describe how you could use a single array to implement three stacks.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int Arr[ 100 ];
+
+// Fixed allocation
+// Use [ 0, N/3 ) for stack1
+// Use [ N/3, 2N/3 ) for stack2
+// Use [ 2N/3 N ] for stack3
+//
+// keep fn to check for full before doing push
+// and empty before doing pop.
+// In full and empty check for limits definded for stack.
+
+int main() {
+  return 0;
+}
