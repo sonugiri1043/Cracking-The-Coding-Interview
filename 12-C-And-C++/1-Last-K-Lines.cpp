50906343b7c8e7fb21d1f5d8e560f320cbeedc39
Sonu Giri <sonugiri1043@gmail.com>
2019-02-02 22:21:44 +0530

    print the last K lines of an input file using C++.

--- 12-C-And-C++/1-Last-K-Lines.cpp
+++ 12-C-And-C++/1-Last-K-Lines.cpp
@@ -8,6 +8,28 @@
   ofstream: Stream class to write on files.
   ifstream: Stream class to read from files.
   fstream: Stream class to both read and write from/to files.
+
+  These classes are derived directly or indirectly from the classes
+  istream and ostream. We have already used objects whose types were
+  these classes: cin is an object of class istream and cout is an object
+  of class ostream. Therefore, we have already been using classes that are
+  related to our file streams. And in fact, we can use our file streams
+  the same way we are already used to use cin and cout, with the only
+  difference that we have to associate these streams with physical files.
+  Let's see an example:
+
+  // basic file operations
+  #include <iostream>
+  #include <fstream>
+  using namespace std;
+  
+  int main () {
+    ofstream myfile;
+    myfile.open ("example.txt");
+    myfile << "Writing this to a file.\n";
+    myfile.close();
+    return 0;
+  }
  */
 
 #include <iostream>
