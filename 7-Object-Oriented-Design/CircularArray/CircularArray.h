cd58183a855bdbf46acd6499643ec5f05992d956
Sonu Giri <sonugiri1043@gmail.com>
2019-03-12 01:37:22 +0530

    Circular Array skeleton

---
+++ 7-Object-Oriented-Design/CircularArray/CircularArray.h
@@ -0,0 +1,10 @@
+/*
+  Circular Array:
+  Implement a CircularArray class that supports an array-like data structure which
+  can be efficiently rotated. If possible, the class should use a generic type
+  ( also called a template ), and should support iteration via the standard
+  for( Obj o : circularArray ) notation.
+ */
+
+class CircularArray {
+};
---
+++ 7-Object-Oriented-Design/CircularArray/CircularArrayTest.cpp
@@ -0,0 +1,5 @@
+#include "CircularArray.h"
+
+int main() {
+  return 0;
+}
