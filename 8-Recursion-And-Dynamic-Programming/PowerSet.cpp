51448ca02c20d146746010c678f4ffd5881fe396
Sonu Giri <sonugiri1043@gmail.com>
2019-04-01 22:28:01 +0530

    Solved power set usign Combinatorics

--- 8-Recursion-And-Dynamic-Programming/PowerSet.cpp
+++ 8-Recursion-And-Dynamic-Programming/PowerSet.cpp
@@ -1,3 +1,5 @@
+// g++ PowerSet.cpp -o <output-file> --std=c++14
+
 /*                                                                                                                                           
   Power Set: Write a method to return all subsets of a set.                                                                                  
                                                                                                                                              
@@ -12,4 +14,32 @@
   Space/Time complexity: O( n2^n ).                                                                                                          
 */
 
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void getPowerSetV1( vector< char > & origSet ) {
+  // Iterate through all no's from 0 - (2^(n)-1).
+  // For each no if ith bit is set then include ith element from original set
+  int noOfSets = 1 << origSet.size();
+  for( int i=0; i < noOfSets; i++ ) {
+    for( int j=0; j < origSet.size(); j++ ) {
+      if( i & ( 1 << j ) ) {
+	cout<< origSet[ j ] << " ";
+      } else {
+	cout<< "  ";
+      }
+    }
+    cout<< endl;
+  }
+}
+
+int main() {
+  vector< char > origSet = { 'a', 'b', 'c', 'd' ,'e' };
+  getPowerSetV1( origSet );
+  return 0;
+}
 
