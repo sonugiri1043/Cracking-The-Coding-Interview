d113dd6378cf1d5b38e3c8e08ce2abe6dcf923fa
Sonu Giri <sonugiri1043@gmail.com>
2019-04-02 00:06:42 +0530

    Coins problem

---
+++ 8-Recursion-And-Dynamic-Programming/Coins.cpp
@@ -0,0 +1,11 @@
+/*
+  Coins:
+  Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents),
+  and pennies (1 cent), write code to calculate the number of ways of representing n cents.
+ */
+
+#include <iostream>
+
+int main() {
+  return 0;
+}
