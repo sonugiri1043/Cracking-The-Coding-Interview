2a38c13d73ee94200d92c0e96566596ea2797a4e
Sonu Giri <sonugiri1043@gmail.com>
2019-04-03 20:06:24 +0530

    Recursive Multiplication without using * skeleton

---
+++ 8-Recursion-And-Dynamic-Programming/RecursiveMultiply.cpp
@@ -0,0 +1,23 @@
+/*
+  Recursive Multiply:
+  Write a recursive function to multiply two positive integers without using the * operator.
+  You can use addition, subtraction, and bit shifting, but you should minimize the number of
+  those operations.
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int multiply( int a, int b ) {
+  int result = 0;
+  return result;
+}
+
+int main() {
+  int a = 15;
+  int b = 12;
+  cout<< multiply( a, b ) << endl;
+  return 0;
+}
