77073b176aeb887a31eb5c6cfed9e8d2f20aae20
Sonu Giri <sonugiri1043@gmail.com>
2019-04-03 21:50:48 +0530

    Multiply without using *

--- 8-Recursion-And-Dynamic-Programming/RecursiveMultiply.cpp
+++ 8-Recursion-And-Dynamic-Programming/RecursiveMultiply.cpp
@@ -10,14 +10,21 @@
 using std::cout;
 using std::endl;
 
+// Assumes b is smaller than a
 int multiply( int a, int b ) {
   int result = 0;
+  while( b ) {
+    if( b & 1 ) {
+      result += a;
+    }
+    b = b >> 1;
+    a = a << 1;
+  }
   return result;
 }
 
 int main() {
-  int a = 15;
-  int b = 12;
-  cout<< multiply( a, b ) << endl;
+  cout<< multiply( 15, 12 ) << endl;
+  cout<< multiply( 35, 31 ) << endl;
   return 0;
 }
