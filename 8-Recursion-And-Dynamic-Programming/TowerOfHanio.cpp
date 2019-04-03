0cb0af4f23e68391de0ec0825d7a08713fad7566
Sonu Giri <sonugiri1043@gmail.com>
2019-04-03 11:55:18 +0530

    Tower of hanoi code

--- 8-Recursion-And-Dynamic-Programming/TowerOfHanio.cpp
+++ 8-Recursion-And-Dynamic-Programming/TowerOfHanio.cpp
@@ -16,5 +16,23 @@
 using std::cout;
 using std::endl;
 
+// Moves all disc from tower 1 to tower 3
+void moveDisc( int noOfDisc, int srcTower, int bufferTower, int dstTower ) {
+  if( noOfDisc <= 0 ) {
+    return;
+  }
+  // move top n-1 from source to buffer, so buffer is new dst
+  moveDisc( noOfDisc - 1, srcTower, dstTower, bufferTower );
+  // move nth disc to destination
+  cout<< "Moving disc " << noOfDisc << " from tower " << srcTower << " to tower " << dstTower << endl;
+  // move n-1 discs from buffer to dest tower
+  moveDisc( noOfDisc - 1, bufferTower, srcTower, dstTower );
+}
+
 int main() {
+  int n = 5;
+  // srcTower = 1
+  // bufferTower = 2
+  // dstTower = 3
+  moveDisc( n, 1, 2, 3 );
 }
