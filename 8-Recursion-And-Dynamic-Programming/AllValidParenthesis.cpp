e3848f7ab8a333a8f0613ca9aae684fe65f683c5
Sonu Giri <sonugiri1043@gmail.com>
2019-04-03 19:28:06 +0530

    Generate all valid parenthesis

--- 8-Recursion-And-Dynamic-Programming/AllValidParenthesis.cpp
+++ 8-Recursion-And-Dynamic-Programming/AllValidParenthesis.cpp
@@ -6,3 +6,29 @@
   Input: 3
   Output: ( ( ( ) ) ), ( ( ) ( ) ), ( ( ) ) ( ), ( ) ( ( ) ), ( ) ( ) ( )
  */
+
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+void balancedParen( int open, int close, int n, string paren ) { 
+  if( close == n ) { // complete balanced parenthesis
+    cout<< paren << endl;
+    return;
+  }
+  if( open < n ) {
+    balancedParen( open + 1, close, n, paren + "(" );
+  } 
+  if( open > close ) {
+    balancedParen( open, close + 1, n, paren + ")" );
+  }
+}
+
+int main() {
+  int n = 3;
+  balancedParen( 0, 0, n, "" );
+  return 0;
+}
