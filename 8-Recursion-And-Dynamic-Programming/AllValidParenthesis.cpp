adfcf045f0580a4c172f2bda5bb61b5eb1a681c8
Sonu Giri <sonugiri1043@gmail.com>
2019-04-03 14:32:16 +0530

    All valid parenthesis

---
+++ 8-Recursion-And-Dynamic-Programming/AllValidParenthesis.cpp
@@ -0,0 +1,8 @@
+/*
+  Parens:
+  Implement an algorithm to print all valid ( e.g., properly opened and closed ) combinations
+  of n pairs of parentheses.
+  EXAMPLE
+  Input: 3
+  Output: ( ( ( ) ) ), ( ( ) ( ) ), ( ( ) ) ( ), ( ) ( ( ) ), ( ) ( ) ( )
+ */
