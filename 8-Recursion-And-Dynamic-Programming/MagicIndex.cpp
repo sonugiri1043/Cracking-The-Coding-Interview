72ec583a6db1b9b47ee29e6e6ddeada23caf68dd
Sonu Giri <sonugiri1043@gmail.com>
2019-04-04 09:30:06 +0530

    Magic Index problem

---
+++ 8-Recursion-And-Dynamic-Programming/MagicIndex.cpp
@@ -0,0 +1,17 @@
+/*
+  Magic Index:
+  A magic index in an array A[ 0 ... n - 1 ] is defined to be an index such that A[ i ] = i.
+  Given a sorted array of distinct integers, write a method to find a magic index, if one exists,
+  in array A.
+  FOLLOW UP
+  What if the values are not distinct?
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  return 0;
+}
