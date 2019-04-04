49c5b4235efffd804a18dfb8508578a822c5a532
Sonu Giri <sonugiri1043@gmail.com>
2019-04-04 09:40:51 +0530

    Boolean Evaluation problem

---
+++ 8-Recursion-And-Dynamic-Programming/BooleanEvaluation.cpp
@@ -0,0 +1,9 @@
+/*
+  Boolean Evaluation:
+  Given a boolean expression consisting of the symbols 0 (false), 1 (true), & (AND), | (OR), and
+  ^ (XOR), and a desired boolean result value result, implement a function to count the number of
+  ways of parenthesizing the expression such that it evaluates to result.
+  EXAMPLE
+  countEval( "1^0|0|1", false ) -> 2
+  countEval( "0 & 0 & 0 & 1^1|0", true ) -> 10
+ */
