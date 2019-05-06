65b6ae1013a519ab6568f1ace2cbeeb23049ce00
Sonu Giri <sonugiri1043@gmail.com>
2019-05-06 09:47:28 +0530

    Given 2 set find a pair of number with smallest difference.

---
+++ 16-Moderate/6-Smallest-Difference.cpp
@@ -0,0 +1,17 @@
+/*
+  Smallest Difference:
+  Given two arrays of integers, compute the pair of values (one value in each
+  array) with the smallest (non-negative) difference. Return the difference.
+  EXAMPLE
+  Input { 1, 3, 15, 11,2 }, {23, 127, 235, 19, 8}
+  Output 3. That is, the pair (11, 8).
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  return 0;
+}
