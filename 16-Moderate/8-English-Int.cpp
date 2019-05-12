a27207ec0be0c4004d0591a5f25bc9acc003a9b1
Sonu Giri <sonugiri1043@gmail.com>
2019-05-12 19:36:54 +0530

    print integers in english

---
+++ 16-Moderate/8-English-Int.cpp
@@ -0,0 +1,12 @@
+/* English Int:
+   Given any integer, print an English phrase that describes the integer
+   ( e.g. "One Thousand, Two Hundred Thirty Four"). */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+  return 0;
+}
