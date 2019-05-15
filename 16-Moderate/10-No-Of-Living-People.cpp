3b97de21282d30fc41abf0fcea2e6ea11dd1df68
Sonu Giri <sonugiri1043@gmail.com>
2019-05-15 23:07:47 +0530

    Max no of people alive in a year given date of birth and death

--- 16-Moderate/10-No-Of-Living-People.cpp
+++ 16-Moderate/10-No-Of-Living-People.cpp
@@ -11,10 +11,12 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 
 using std::cout;
 using std::endl;
 using std::vector;
+using std::set;
 
 struct Person {
   int yearOfBirth;
@@ -26,23 +28,51 @@ struct Person {
   }
 };
 
+struct Year {
+  int year;
+  bool birth;
+
+  Year( int year_, bool birth_ ) { 
+    year = year_;
+    birth = birth_;
+  }
+  
+  bool operator< ( const Year &y ) const {
+    return year < y.year;
+  }
+};
+
 int main() {
   vector<Person> persons = { Person( 12, 15 ), Person( 20, 90 ), Person( 10, 98 ),
 			     Person( 01, 72 ), Person( 10, 98 ), Person( 23, 82 ),
 			     Person( 13, 98 ), Person( 90, 98 ), Person( 83, 99 ),
 			     Person( 75, 94 ) };
+
+  vector< Year > years;
+  for( auto person : persons ) {
+    years.push_back( Year( person.yearOfBirth, true ) );
+    // added 1 since death year is also included.
+    years.push_back( Year( person.yearOfDeath + 1, false ) );
+  }
+
+  std::sort( years.begin(), years.end() );
+
+  int maxPeople = 0;
+  int maxYear = 0;
+  int tmp = 0;
+  for( auto y : years ) {
+    if( y.birth ) {
+      tmp += 1;
+      if( tmp > maxPeople ) {
+	maxPeople = tmp;
+	maxYear = y.year;
+      }
+    } else {
+      tmp -= 1;
+    }
+  }
+
+  cout<< "Max people alive " << maxPeople << " in year " << maxYear << endl;
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
