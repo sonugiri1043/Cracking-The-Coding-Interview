aee509d1848ea03e4368ca1f166d57e6336dc8ea
Sonu Giri <sonugiri1043@gmail.com>
2019-05-27 13:38:44 +0530

    multiply, subtract and divide by only using add operation.

---
+++ 16-Moderate/9-Operations.cpp
@@ -0,0 +1,5 @@
+/*
+  Operations:
+  Write methods to implement the multiply, subtract, and divide operations for integers.
+  The results of all of these are integers. Use only the add operator.
+*/
