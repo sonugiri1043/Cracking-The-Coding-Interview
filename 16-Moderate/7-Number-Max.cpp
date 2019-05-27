82d916a754c11458d8eefb02717d938418c9a99f
Sonu Giri <sonugiri1043@gmail.com>
2019-05-27 11:48:04 +0530

    Max of two number without using any comparision.

---
+++ 16-Moderate/7-Number-Max.cpp
@@ -0,0 +1,14 @@
+/*
+  Number Max:
+  Write a method that finds the maximum of two numbers. You should not use if-else
+  or any other comparison operator.
+
+  Sol: a and b are two numbers. Get the most significant bit of a - b.
+
+  k = a - b
+  p = most significant bit of k.
+  q = reverse of k
+  return p*a + q*b;
+
+ */
+
