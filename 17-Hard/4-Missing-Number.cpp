7db482d2ee526d9bb3e552d901b6cd5205edef5b
Sonu Giri <sonugiri1043@gmail.com>
2019-05-31 12:05:10 +0530

    missing number

---
+++ 17-Hard/4-Missing-Number.cpp
@@ -0,0 +1,8 @@
+/*
+  Missing Number:
+  An array A contains all the integers from 0 to n, except for one number which
+  is missing. In this problem, we cannot access an entire integer in A with a single operation. The
+  elements of A are represented in binary, and the only operation we can use to access them is "fetch
+  the jth bit of A[i]," which takes constant time. Write code to find the missing integer. Can you do it
+  in O(n) time?
+ */
