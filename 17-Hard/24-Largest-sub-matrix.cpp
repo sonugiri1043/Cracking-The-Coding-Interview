5c494813d7da8f298fba8db372b3552c94a9f0fc
Sonu Giri <sonugiri1043@gmail.com>
2019-06-05 20:55:22 +0530

    Sub matrix with largest sum

--- 17-Hard/24-Largest-sub-matrix.cpp
+++ 17-Hard/24-Largest-sub-matrix.cpp
@@ -1,7 +1,7 @@
 /*
   Max Submatrix: Given an NxN matrix of positive and negative integers, write code to find the
   submatrix with the largest possible sum.
- */
+*/
 
 // g++ 24-Largest-sub-matrix.cpp --std=c++14
 
@@ -58,23 +58,18 @@ int getMaxSubMatrixBruteForce( int arr[][5], int row, int col ) {
 /***************************************************************************************/
 
 int kadaneAlgo( vector<int> v, int &start, int &end ) {
-  if( v.size() == 1 ) {
-    start = 0;
-    end = 0;
-    return v[0];
-  } 
-
   int maxSum = INT_MIN;  
-  int sum = v[0];
+  int sum = 0;
   int localStart = 0;
   int localEnd = 0;
-  for( int i = 1; i < v.size(); ++i ) {
+  for( int i = 0; i < v.size(); ++i ) {
     if( v[i] + sum > v[i] ) {
       sum += v[i];
       localEnd = i;
     } else {
       sum = v[i];
       localStart = i;
+      localEnd = i;
     }
 
     if( sum > maxSum ) {
@@ -83,25 +78,49 @@ int kadaneAlgo( vector<int> v, int &start, int &end ) {
       maxSum = sum;
     }
   }
+
+  // for debug
+  for( auto i : v ) {
+    cout << i << " ";
+  }
+  cout << endl;
   cout<< "Max sum: " << maxSum << " start:" << start << " end:" << end << endl;
+
   return maxSum;
 }
 
 int getMaxSubMatrixSum( int arr[][5], int row, int col ) {
   int maxSum = INT_MIN;
-  int top, bottom, left, right; // position of sub-matrix
-  for( int i = 0; i < col; i++ ) {
-    for( int j = i; j < col; j++ ) {
+  int top, bottom, left_index, right_index; // position of sub-matrix
+  int local_start, local_end;
+  int local_sum = INT_MIN;
+
+  for( int left = 0; left < col; left++ ) {
+    vector< int > v( row, 0 );
+    for( int right = left; right < col; right++ ) {
+      for( int k = 0; k < row; ++k ) {
+	v[k] += arr[k][right];
+      }
+      local_sum = kadaneAlgo( v, local_start, local_end );
+      if( local_sum >= maxSum ) {
+	// update global sum and position
+	maxSum = local_sum;
+	left_index = left;
+	right_index = right;
+	top = local_start;
+	bottom = local_end;
+      }
     }
   }
   return maxSum;
 }
 
 int main() {
-  int arr[4][5] = { { 1, 2, -1, -4, -20 },
-		    { -8, -3, 4, 2, 1 },
-		    { 3, 8, 10, 1, 3 },
-		    { -4, -1, 1, 7, -6 } };
+  int arr[4][5] = { {  1,  2, -1, -4, -20 },
+		    { -8, -3,  4,  2,   1 },
+		    {  3,  8, 10,  1,   3 },
+		    { -4, -1,  1,  7,  -6 } };
+
   cout<< "-------------Brute Force approach----------" << endl;
   cout << getMaxSubMatrixBruteForce( arr, 4, 5 ) << endl;
 
@@ -110,9 +129,13 @@ int main() {
   int end;
   vector<int> v1 = {-8, -3, 4, 2, 1};
   vector<int> v2 = { -4, -1, 1, 7, -6 };
+  vector<int> v3 = { 1, -1, 0, 0 };
   kadaneAlgo( v1, start, end );
   kadaneAlgo( v2, start, end );
+  kadaneAlgo( v3, start, end );
+
+  cout<< "----------------------------------------" << endl;
   cout << "Max sub-matix sum using kadane algo." << endl;
-  //cout << getMaxSubMatrixSum( arr, 4, 5 ) << endl;
+  cout << getMaxSubMatrixSum( arr, 4, 5 ) << endl;
   return 0;
 }
